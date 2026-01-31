304ee16ae815d4c07cf4cd8387605b3f82844e63
Tobias Franzen <tobias.franzen@durham.ac.uk>
2026-01-31 22:03:41 +0000

    make settings a class

--- PyOpticL/settings.py
+++ PyOpticL/settings.py
@@ -1,14 +1,15 @@
-metric_hardware = False
-preferred_bolt_tag = "imperial"
+class settings:
 
-def set_hardware_preference(system: str):
-    global metric_hardware
+    metric_hardware = False
+    preferred_bolt_tag = "imperial"
 
-    if system.lower() == "metric":
-        metric_hardware = True
-        preferred_bolt_tag = "metric"
-    elif system.lower() == "imperial":
-        metric_hardware = False
-        preferred_bolt_tag = "imperial"
-    else:
-        raise ValueError("Invalid system preference. Use 'metric' or 'imperial'.")
+    def set_hardware_preference(system: str):
+
+        if system.lower() == "metric":
+            metric_hardware = True
+            preferred_bolt_tag = "metric"
+        elif system.lower() == "imperial":
+            metric_hardware = False
+            preferred_bolt_tag = "imperial"
+        else:
+            raise ValueError("Invalid system preference. Use 'metric' or 'imperial'.")
